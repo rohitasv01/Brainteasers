fbc57a1e800d1aa74db933fee6c8de04c652cf15
rohitasv01 <rohitasv01@gmail.com>
2026-03-21 12:35:50 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (31.65%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -6,10 +6,10 @@ public:
         for(int i=0;i<n;i++)
         {
             hash[s[i]-'a']=i;
-            if(hash[0]>=0 && hash[1]>=0 && hash[2]>=0 )
-            {
+           // if(hash[0]>=0 && hash[1]>=0 && hash[2]>=0 )
+            //{
                 count +=1+min(hash[0],min (hash[1],hash[2]));
-            }
+           // }
         }
         return count;
     }
