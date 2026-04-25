348ca4f2fd05cdffbc615a63b67b975469a07eac
rohitasv01 <rohitasv01@gmail.com>
2026-04-25 12:25:18 +0530

    Time: 0 ms (100.00%) | Memory: 37.3 MB (65.44%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
@@ -9,24 +9,63 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ class BSTIterator
+ {
+    stack<TreeNode*> st;
+    bool reverse;
+    public:
+    BSTIterator(TreeNode* node, bool isReverse)
+    {
+        reverse=isReverse;
+        pushAll(node);
+    }
+    int next()
+    {
+        TreeNode* temp=st.top();
+        st.pop();
+        if(!reverse)
+        {
+            if(temp->right) pushAll(temp->right);
+        }
+        else
+        {
+            if(temp->left)  pushAll(temp->left);
+        }
+        return temp->val;
+    }
+    void pushAll(TreeNode* node)
+    {
+        if(!reverse)
+        {
+            while(node)
+            {
+               st.push(node); 
+               node=node->left;
+            }
+        }
+        else
+        {
+            while(node)
+            {
+                st.push(node);
+                node=node->right;
+            }
+        }
+    }
+ };
 class Solution {
 public:
-    void traverse(TreeNode* node,vector<int> &inorder)
-    {
-        if(node==NULL)  return ;
-        traverse(node->left,inorder);
-        inorder.push_back(node->val);
-        traverse(node->right,inorder);
-    }
     bool findTarget(TreeNode* root, int k) {
-        vector<int> inorder;
-        traverse(root,inorder);
-        int p1=0,p2=inorder.size()-1;
-        while(p1<p2)
+        if(root==NULL)  return false;
+        BSTIterator l(root,false);
+        BSTIterator r(root,true);
+        int i=l.next();
+        int j=r.next();
+        while(i<j)
         {
-            if(inorder[p1]+inorder[p2]==k)  return true;
-            else if(inorder[p1]+inorder[p2]<k)  p1++;
-            else    p2--;
+            if(i+j==k)  return true;
+            else if(i+j<k)  i=l.next();
+            else    j=r.next();
         }
         return false;
     }
