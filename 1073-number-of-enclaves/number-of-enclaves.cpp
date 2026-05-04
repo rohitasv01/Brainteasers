30d724ade6fad77aaf51a1f637dae5f949563e41
rohitasv01 <rohitasv01@gmail.com>
2026-05-04 19:00:35 +0530

    Time: 8 ms (60.73%) | Memory: 28.9 MB (93.83%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -3,39 +3,38 @@ public:
     int numEnclaves(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        vector<vector<int>> visited(n,vector<int>(m,-1));
         queue<pair<int,int>>q;
         //first and last row
         for(int i=0;i<m;i++)
         {
-            if(grid[0][i]==1)  
+            if(grid[0][i]==1)   
             {
+                grid[0][i]=-1;
                 q.push({0,i});
-                visited[0][i]=1;
-            } 
+            }
             if(grid[n-1][i]==1)
             {
+                grid[n-1][i]=-1;
                 q.push({n-1,i});
-                visited[n-1][i]=1;
-            } 
+            }
         }
-        //first and last column
+        //first and last col
         for(int i=0;i<n;i++)
         {
             if(grid[i][0]==1)
             {
+                grid[i][0]=-1;
                 q.push({i,0});
-                visited[i][0]=1;
-            }   
-            if(grid[i][m-1]==1) 
+            }
+            if(grid[i][m-1]==1)
             {
+                grid[i][m-1]=-1;
                 q.push({i,m-1});
-                visited[i][m-1]=1;
             }
         }
         int dx[4]={1,0,-1,0};
         int dy[4]={0,1,0,-1};
-        while(q.size()!=0)
+        while(q.size())
         {
             auto[x,y]=q.front();
             q.pop();
@@ -43,10 +42,10 @@ public:
             {
                 int nx=x+dx[i];
                 int ny=y+dy[i];
-                if(nx>=0 && ny>=0 && nx<n && ny<m && visited[nx][ny]==-1 && grid[nx][ny]==1)
+                if(nx>=0 && ny>=0 && nx<n && ny<m && grid[nx][ny]==1)
                 {
+                    grid[nx][ny]=-1;
                     q.push({nx,ny});
-                    visited[nx][ny]=1;
                 }
             }
         }
@@ -55,8 +54,7 @@ public:
         {
             for(int j=0;j<m;j++)
             {
-                if(grid[i][j]==1)   count++;
-                if(visited[i][j]==1)    count--;
+                if(grid[i][j]==1)   count ++;
             }
         }
         return count;
