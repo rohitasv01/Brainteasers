a8a9b787f6a00999588fdd707741a599acb89a63
rohitasv01 <rohitasv01@gmail.com>
2026-05-04 18:28:27 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (96.73%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
-    int n,m;
     void dfs(int i,int j,vector<vector<char>>& board)
     {
+        int n=board.size();
+        int m=board[0].size();
         if(i<0 || j<0 || i>=n || j>=m || board[i][j]!='O')  return ;
         board[i][j]='S';
         dfs(i+1,j,board);
@@ -11,24 +12,26 @@ public:
         dfs(i,j-1,board);
     }
     void solve(vector<vector<char>>& board) {
-        n=board.size();
-        m=board[0].size();
+        int n=board.size();
+        int m=board[0].size();
+        //first and last row 
         for(int i=0;i<m;i++)
         {
-            if(board[0][i]=='O')    dfs(0,i,board);
-            if(board[n-1][i]=='O')  dfs(n-1,i,board);
-        }   
+            if(board[0][i]=='O')     dfs(0,i,board);
+            if(board[n-1][i]=='O')   dfs(n-1,i,board);
+        }
+        //first and last col
         for(int i=0;i<n;i++)
         {
-            if(board[i][0]=='O')    dfs(i,0,board);
-            if(board[i][m-1]=='O')  dfs(i,m-1,board);
+            if(board[i][0]=='O')     dfs(i,0,board);
+            if(board[i][m-1]=='O')   dfs(i,m-1,board);
         }
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                if(board[i][j]=='O')    board[i][j]='X';
-                if(board[i][j]=='S')    board[i][j]='O';
+                if(board[i][j]=='O')     board[i][j]='X';
+                if(board[i][j]=='S')     board[i][j]='O';
             }
         }
     }
