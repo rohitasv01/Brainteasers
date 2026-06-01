426eecdc74a0182a3f82e6a8ca4543a6ecb43f88
rohitasv01 <rohitasv01@gmail.com>
2026-06-01 18:30:27 +0530

    Time: 23 ms (42.44%) | Memory: 46.6 MB (52.17%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -1,53 +1,52 @@
 class Solution {
 public:
-
-    void dfs(int node, vector<vector<int>>& adj, vector<int>& visited)
+    class DisjointSet
     {
-        visited[node] = 1;
-
-        for(auto neighbour : adj[node])
+        public:
+        vector<int> parent,size;
+        DisjointSet(int n)
         {
-            if(!visited[neighbour])
+            parent.resize(n+1);
+            size.resize(n+1,1);
+            for(int i=0;i<n;i++)    parent[i]=i;
+        }
+        int findParent(int node)
+        {
+            if(node==parent[node])  return node;
+            return parent[node]=findParent(parent[node]);
+        }
+        void unionBySize(int u,int v)
+        {
+            int pu=findParent(u);
+            int pv=findParent(v);
+            if(pu==pv)  return ;
+            if(size[pu]<size[pv])
             {
-                dfs(neighbour, adj, visited);
+                parent[pu]=pv;
+                size[pv]+=size[pu];
+            }
+            else
+            {
+                parent[pv]=pu;
+                size[pu]+=size[pv];
             }
         }
-    }
-
+    };
     int makeConnected(int n, vector<vector<int>>& connections) {
-
-        // Not enough edges
-        if(connections.size() < n-1)
-            return -1;
-
-        // Build graph
-        vector<vector<int>> adj(n);
-
-        for(auto it : connections)
+        DisjointSet ds(n);
+        int extra=0,components=0;
+        for(auto it:connections)
         {
-            int u = it[0];
-            int v = it[1];
-
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+            int u=it[0];
+            int v=it[1];
+            if(ds.findParent(u)==ds.findParent(v))  extra++;
+            else    ds.unionBySize(u,v);
         }
-
-        // Visited array
-        vector<int> visited(n,0);
-
-        int components = 0;
-
-        // Count connected components
-        for(int i=0;i<n;i++)
+        for(int i=0;i<n;i++)    
         {
-            if(!visited[i])
-            {
-                components++;
-                dfs(i, adj, visited);
-            }
+            if(ds.parent[i]==i)    components++;
         }
-
-        // Minimum cables needed
-        return components - 1;
+        if(extra>=components-1)   return components-1;
+        else return -1;
     }
 };
