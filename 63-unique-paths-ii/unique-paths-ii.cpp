eb36dba0f963073a4d176d7882087add8a3b3ee7
rohitasv01 <rohitasv01@gmail.com>
2026-06-23 17:24:47 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (85.22%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
-    int solve(int i,int j,vector<vector<int>> &dp, vector<vector<int>> &obstacleGrid)
-    {
-        if(i<0 ||j<0)   return 0;
-        if(obstacleGrid[i][j]==1)   return 0;
-        if(i==0 && j==0)    return 1;
-        if(dp[i][j]!=-1)    return dp[i][j];
-        int up=solve(i-1,j,dp,obstacleGrid);
-        int left=solve(i,j-1,dp,obstacleGrid);
-        return dp[i][j]=up+left;
-    }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int n=obstacleGrid.size();
         int m=obstacleGrid[0].size();
-        vector<vector<int>> dp(n,vector<int> (m,-1));
-        return solve(n-1,m-1,dp,obstacleGrid);
+        vector<int> prev(m,0);
+        for(int i=0;i<n;i++)
+        {
+            vector<int> curr(m,0);
+            for(int j=0;j<m;j++)
+            {
+                if(obstacleGrid[i][j]==1)   
+                {
+                    curr[j]=0;
+                }
+                else if(i==0 && j==0)
+                {
+                    curr[j]=1;
+                }
+                else
+                {
+                    int up=0,left=0;
+                    if(i>0) up=prev[j];
+                    if(j>0) left=curr[j-1];
+                    curr[j]=up+left;
+                }
+            }
+            prev=curr;
+        }
+        return prev[m-1];
     }
 };
