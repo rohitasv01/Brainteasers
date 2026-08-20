1a8b2893c154b0037fbaf9ab88c92eca7751a525
rohitasv01 <rohitasv01@gmail.com>
2026-08-20 17:58:44 +0530

    Time: 1 ms (93.35%) | Memory: 11.1 MB (28.44%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
+    //check last occurences no of subtrings will be min of last occurence of all 3 +1
     int numberOfSubstrings(string s) {
-        int n=s.size(),l=0,r=0,count=0;
-        vector <int> hash(3,-1);
+        vector<int> hash(3,-1);
+        int count=0,n=s.size();
         for(int i=0;i<n;i++)
         {
             hash[s[i]-'a']=i;
-           // if(hash[0]>=0 && hash[1]>=0 && hash[2]>=0 )
-            //{
-                count +=1+min(hash[0],min (hash[1],hash[2]));
-           // }
+            count+=1+min(hash[0],min(hash[1],hash[2]));
         }
         return count;
     }
